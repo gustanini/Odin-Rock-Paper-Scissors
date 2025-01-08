31c668e0d9ab2682b0e9ba192a9d6109f388f108
Rafael Pimentel <47717581+gustanini@users.noreply.github.com>
2025-01-08 08:42:48 +0100

    feat: add playRound() function to rps-ui.js

--- rps-ui/rps-ui.js
+++ rps-ui/rps-ui.js
@@ -12,11 +12,32 @@ let playerScore = 0;
 let computerScore = 0;
 
 // Function to get computer's selection
-function getMachineChoice() {
+function getComputerChoice() {
     return rps[Math.floor(Math.random() * 3)];
 }
 
 // Function to determine the winner of a round
+function playRound(playerSelection, computerSelection){
+    // bigger number wins
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
+    }
+    // player wins
+    else if (
+        (playerSelection === 0 && computerSelection === 2) || // Rock beats Scissors
+        (playerSelection === 1 && computerSelection === 0) || // Paper beats Rock
+        (playerSelection === 2 && computerSelection === 1)    // Scissors beats Paper
+    ) {
+        // add to playerScore
+        playerScore++;
+        console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+    }
+    else {
+        // add to computerScore
+        computerScore++;
+        console.log(`Opponent wins! ${computerSelection} beats ${playerSelection}!`);
+    }
+}
 
 // Function to handle button clicks
 
